33863e5f5d46b7358123947f8672e857a6374f3a
Olga Shestakovich <olga.shestakovich1@gmail.com>
2021-02-03 16:33:59 +0300

    added Program.cs and Class Animal

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal();
+            string countString = cat.Amount(5).ToString();
+            Console.WriteLine(countString);
+        }
+    }
+}
---
+++ Services/Animal.cs
@@ -0,0 +1,15 @@
+namespace ProjectNew
+{
+    public class Animal
+    {
+        int food = 0;
+        public int Amount(int count)
+        {
+            this.food += count;
+            return count;
+        }
+
+    }
+
+    
+}
